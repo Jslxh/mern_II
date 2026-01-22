a89b2e632098d3348ebe332be3f872a65324c785
Jslxh <shreejayalakshmis@gmail.com>
2026-01-22 15:49:56 +0530

    mern_II

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const router=express.Router()
+const {createTask, getTask,getTaskById,updateTask,updateTaskPatch,deleteTask}=require('../controllers/taskControllers')
+const {protect}=require('../middleware/authmiddleware')
+router.post('/createTask',protect,createTask)
+router.get('/getTask',protect,getTask)
+router.get('/getTask/:id',protect,getTaskById)
+router.put('/updateTask/:id',protect,updateTask)
+router.patch('/updateTaskPatch/:id',protect,updateTaskPatch)
+router.delete('/deleteTask/:id',protect,deleteTask)
+module.exports=router
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app=express()
+const mongoose = require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config()
+//const app = express()
+app.use(express.json())
+mongoose.connect(
+    process.env.MONGO_URI
+).then(()=>{
+console.log("MongoDB connected successfully");
+}).catch((err)=>{console.log(err)})
+
+app.use('/auth',require('./routes/authroutes'))
+app.use('/task',require('./routes/taskRoutes'))
+app.get('/api',(req,res)=>{
+    res.send('Hi!! From express')
+})
+app.post('/api',(req,res)=>{
+const temp=req.body;
+res.send(temp);
+})
+app.listen(4000,()=>{
+    console.log("server running on 4000")
+});
