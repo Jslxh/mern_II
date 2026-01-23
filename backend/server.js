d37598c7e19bda322702c2790d67cabba94f4251
Jslxh <shreejayalakshmis@gmail.com>
2026-01-23 09:09:29 +0530

    mern_II

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app=express()
 const mongoose = require('mongoose')
 const dotenv=require('dotenv')
+const cors = require('cors')
 dotenv.config()
-//const app = express()
+app.use(cors())
 app.use(express.json())
 mongoose.connect(
     process.env.MONGO_URI
